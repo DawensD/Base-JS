02cdb9817590a065fb3d741ed6ca41a3570bccac
Dawens <156696312+DawensD@users.noreply.github.com>
2025-02-26 20:20:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,45 @@
-// Création éelement html avec javascript DOM
-
-const titre = document.createElement("h1")
+// Création d'un élément HTML avec le DOM
+const titre = document.createElement("h1");
 titre.textContent = "Bonjour, je suis Dawens";
 
-const main = document.querySelector("main")
+// Sélection de l'élément <main> et ajout du titre
+const main = document.querySelector("main");
 main.appendChild(titre);
 
-// possibilité d'ajouter des codes html dans une balise et l'integrer dans un conteneur
-
-const divCarte = document.createElement("div")
-divCarte.classList.add("carte")
+// Création d'un conteneur div pour afficher des informations dynamiques
+const divCarte = document.createElement("div");
+divCarte.classList.add("carte");
 const age = 20;
 
+// Ajout de contenu HTML dans la div créée
 const balises = `
-
-<h3> <i> Bienvue sur mon site, j'ai ${age} ans </i> </h3 >
-
-`
-divCarte.innerHTML = balises; //
+<h3> <i> Bienvenue sur mon site, j'ai ${age} ans </i> </h3>
+`;
+divCarte.innerHTML = balises;
 main.appendChild(divCarte);
 
-// Sélection Multiple (titres, iamges etc ...)
-
-const titres = document.querySelectorAll("h2")
-
+// Sélection multiple d'éléments <h2>
+const titres = document.querySelectorAll("h2");
 let nombre = 0;
 
-titres.forEach( (t) =>{
+titres.forEach((t) => {
     nombre += 1;
     t.textContent = `Objet n° ${nombre}`;
 });
 
-// Ajout des Evenements (evenements javascript sur internet)
-// Cas 1 : Double Clique
-
-const btn = document.querySelector(".btn")
+// Ajout d'événements interactifs
+// Cas 1 : Clic sur le bouton "Acheter"
+const btn = document.querySelector(".btn");
 btn.addEventListener("click", () => {
-    alert("objet acheté"); //notification "objet acheté"
+    alert("objet acheté"); // Notification indiquant un achat
 });
 
-// Cas 2 : Formulaire (le contenue ecrit apparait en direct)
+// Cas 2 : Mise à jour dynamique des titres en fonction du champ de texte
 const input = document.querySelector(".nom");
 input.addEventListener("keyup", (e) => {
     console.log(e.target.value);
-
-    titres.forEach( (t) =>{
+    
+    titres.forEach((t) => {
         t.textContent = `Vous écrivez : ${e.target.value}`;
-    });    
+    });
 });
